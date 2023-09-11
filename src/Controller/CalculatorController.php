25696001c8514699e7b39d4bdec4808e5742d1a9
Daniel Jablonski <daniel@jablonski.ovh>
2023-09-11 10:03:40 +0200

    added float support

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -18,58 +18,30 @@ class CalculatorController extends AbstractController
     }
 
     #[Route('/api/add/{firstNumber}/{secondNumber}', name: 'api_calculator_add')]
-    public function add(int $firstNumber, int $secondNumber): Response
+    public function add(string $firstNumber, string $secondNumber): Response
     {
-        $result = $this->calculator->add($firstNumber, $secondNumber);
-
-        $return = array('first number' => $firstNumber, 
-                        'second number' => $secondNumber,
-                        'result' => $result,
-                        'error' => ''
-                    );
-
+        $return = $this->calculator->add($firstNumber, $secondNumber);
         return new JsonResponse($return);
     }
 
     #[Route('/api/substract/{firstNumber}/{secondNumber}', name: 'api_calculator_substract')]
-    public function substract(int $firstNumber, int $secondNumber): Response
+    public function substract(string $firstNumber, string $secondNumber): Response
     {
-        $result = $this->calculator->substract($firstNumber, $secondNumber);
-
-        $return = array('first number' => $firstNumber, 
-                        'second number' => $secondNumber,
-                        'result' => $result,
-                        'error' => ''
-                    );
-
+        $return = $this->calculator->substract($firstNumber, $secondNumber);
         return new JsonResponse($return);
     }
 
     #[Route('/api/multiply/{firstNumber}/{secondNumber}', name: 'api_calculator_multiply')]
-    public function multiply(int $firstNumber, int $secondNumber): Response
+    public function multiply(string $firstNumber, string $secondNumber): Response
     {
-        $result = $this->calculator->multiply($firstNumber, $secondNumber);
-
-        $return = array('first number' => $firstNumber, 
-                        'second number' => $secondNumber,
-                        'result' => $result,
-                        'error' => ''
-                    );
-
+        $return = $this->calculator->multiply($firstNumber, $secondNumber);
         return new JsonResponse($return);
     }
 
     #[Route('/api/divide/{firstNumber}/{secondNumber}', name: 'api_calculator_divide')]
-    public function divide(int $firstNumber, int $secondNumber): Response
+    public function divide(string $firstNumber, string $secondNumber): Response
     {
-        $result = $this->calculator->divide($firstNumber, $secondNumber);
-
-        $return = array('first number' => $firstNumber, 
-                        'second number' => $secondNumber,
-                        'result' => $result,
-                        'error' => ''
-                    );
-
+        $return = $this->calculator->divide($firstNumber, $secondNumber);
         return new JsonResponse($return);
     }
 
--- src/Model/Calculator.php
+++ src/Model/Calculator.php
@@ -2,26 +2,120 @@
 
 namespace App\Model;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class Calculator
 {
-    public function add(int $firstNumber, int $secondNumber): int
+    public function add(string $firstNumber, string $secondNumber): array
     {
-        return $firstNumber+$secondNumber;
+        $error = '';
+        $result = 0;
+
+        try {
+            $firstNumber = $this->parseInput($firstNumber);
+            $secondNumber = $this->parseInput($secondNumber);
+            $result = $this->parseOutput($firstNumber+$secondNumber);
+        } catch (\TypeError $e) {
+            $error = 'Nieprawidłowe dane wejściowe przy dodawaniu.';
+        }
+
+        return array('first number' => $firstNumber, 
+            'second number' => $secondNumber,
+            'result' => $result,
+            'error' => $error
+        );
     }
 
-    public function substract(int $firstNumber, int $secondNumber): int
+    public function substract(string $firstNumber, string $secondNumber): array
     {
-        return $firstNumber-$secondNumber;
+        $error = '';
+        $result = 0;
+
+        try {
+            $firstNumber = $this->parseInput($firstNumber);
+            $secondNumber = $this->parseInput($secondNumber);
+            $result = $this->parseOutput($firstNumber-$secondNumber);
+        } catch (\TypeError $e) {
+            $error = 'Nieprawidłowe dane wejściowe przy odejmowaniu.';
+        }
+
+        return array('first number' => $firstNumber, 
+            'second number' => $secondNumber,
+            'result' => $result,
+            'error' => $error
+        );
     }
 
-    public function multiply(int $firstNumber, int $secondNumber): int
+    public function multiply(string $firstNumber, string $secondNumber): array
     {
-        return $firstNumber*$secondNumber;
+        $error = '';
+        $result = 0;
+
+        try {
+            $firstNumber = $this->parseInput($firstNumber);
+            $secondNumber = $this->parseInput($secondNumber);
+            $result = $this->parseOutput($firstNumber*$secondNumber, 'multiply');
+        } catch (\TypeError $e) {
+            $error = 'Nieprawidłowe dane wejściowe przy mnozeniu.';
+        }
+
+        return array('first number' => $firstNumber, 
+            'second number' => $secondNumber,
+            'result' => $result,
+            'error' => $error
+        );
     }
 
-    public function divide(int $firstNumber, int $secondNumber): int
+    public function divide(string $firstNumber, string $secondNumber): array
     {
-        return $firstNumber/$secondNumber;
+        $error = '';
+        $result = 0;
+
+        try {
+            $firstNumber = $this->parseInput($firstNumber);
+            $secondNumber = $this->parseInput($secondNumber);
+            $result = $this->parseOutput($firstNumber/$secondNumber, 'divide');
+        } catch (\TypeError $e) {
+            $error = 'Nieprawidłowe dane wejściowe przy dzieleniu.';
+        } catch (\DivisionByZeroError $e){
+            $error = 'Nie można dzielić przez 0.';
+        }
+
+        return array('first number' => $firstNumber, 
+            'second number' => $secondNumber,
+            'result' => $result,
+            'error' => $error
+        );
     }
 
+    private function parseInput(string $number):int
+    {
+        is_numeric($number);
+        $number = str_replace(',', '.', $number);
+        $number = intval($number * 100) / 100;
+        $number = number_format($number, 2, '.', '');
+        $number = str_replace('.', '', $number);
+        $number = (int)$number;
+        //dd($number);
+        return $number;
+    }
+
+    private function parseOutput(string $number, string $function = ''): float
+    {
+        switch($function)
+        {
+            case 'multiply':
+                $number = $number / 10000.0;
+                break;
+            case 'divide':
+                break;
+            default:
+                $number = $number / 100.0;
+        }
+
+        $number = number_format($number, 2, '.', '');
+        $number = (float)$number;
+        //dd($number);
+        return $number;
+    }
 }
